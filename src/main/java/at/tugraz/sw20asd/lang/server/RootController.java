e8abaebc80a88563039bcc37206a369dac12195d
Michael Schiller <schillerm01+git@gmail.com>
2020-03-25 22:33:39 +0100

    Add back end implementation using file-based storage

--- src/main/java/at/tugraz/sw20asd/lang/server/RootController.java
+++ src/main/java/at/tugraz/sw20asd/lang/server/RootController.java
@@ -1,4 +1,12 @@
 package at.tugraz.sw20asd.lang.server;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RootController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greeting from RootController (default)";
+    }
 }
