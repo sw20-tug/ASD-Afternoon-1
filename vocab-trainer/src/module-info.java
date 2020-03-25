234492f63dab54103bcb079ee8bdf066c70c633c
Eric de Zwart <eric.dezwart@student.tugraz.at>
2020-03-25 17:34:45 +0100

    added maven build and vocab-server

--- vocab-trainer/src/module-info.java
+++ vocab-trainer/src/module-info.java
@@ -2,11 +2,7 @@ module module {
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.fxml;
-	requires javafx.swing;
 	requires javafx.media;
 	requires javafx.graphics;
-	//requires javafx.swt;
-	requires javafx.web;
-	
 	opens main;
 }
