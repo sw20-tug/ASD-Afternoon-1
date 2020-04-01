efedcf80e493564b85af91c18946e8f535d84738
Sarah Cheng <sarah.cheng@student.tugraz.at>
2020-04-01 11:44:35 +0200

    start sprint2

--- src/main/java/at/tugraz/sw20asd/lang/ui/LangUI.java
+++ src/main/java/at/tugraz/sw20asd/lang/ui/LangUI.java
@@ -3,8 +3,6 @@ package at.tugraz.sw20asd.lang.ui;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 
