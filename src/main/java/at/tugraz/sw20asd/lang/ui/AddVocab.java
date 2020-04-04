cb28659bfdaf2e05b63bcdf43d0bc99e26b5213d
Katharina Kronegger <k.kronegger@student.tugraz.at>
2020-04-04 20:53:07 +0200

    logic implemented

--- src/main/java/at/tugraz/sw20asd/lang/ui/AddVocab.java
+++ src/main/java/at/tugraz/sw20asd/lang/ui/AddVocab.java
@@ -1,34 +1,60 @@
 package at.tugraz.sw20asd.lang.ui;
 
+import at.tugraz.sw20asd.lang.model.Vocabulary;
+import at.tugraz.sw20asd.lang.service.VocabularyDAO;
+import at.tugraz.sw20asd.lang.service.VocabularyDAOFileImpl;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+
+import at.tugraz.sw20asd.lang.model.Entry;
+import javafx.scene.paint.Color;
+import org.springframework.util.ObjectUtils;
 
 public class AddVocab extends VBox {
     @FXML
-    private TextField german_field;
+    private TextField from_field;
     @FXML
-    private TextField english_field;
+    private TextField to_field;
     @FXML
-    private Label german_label;
+    private Label from_label;
     @FXML
-    private Label english_label;
-    @FXML
-    private Label title;
+    private Label to_label;
     @FXML
     private Button submit_btn;
     @FXML
     private Button return_btn;
+    @FXML
+    private ComboBox<String> from_choice;
+    @FXML
+    private ComboBox<String> to_choice;
+    @FXML
+    private Label user_info;
 
-    private String german_string;
-    private String english_string;
+    private String from_string;
+    private String to_string;
+
+    private VocabularyDAO vocabularyDAO;
+    private File workingDirectory;
+
+    private int vocabularyID;
+    private Task<Integer> id_task;
 
     public AddVocab(){
         FXMLLoader loader = new FXMLLoader();
@@ -41,27 +67,158 @@ public class AddVocab extends VBox {
             throw new RuntimeException(ex);
         }
     }
-    public void initialize(){
 
-        german_field.textProperty().addListener((observable, oldValue, newValue) -> {
-            german_string = newValue;
+    public void initialize(){
+        workingDirectory = new File("vocabs");
+        vocabularyDAO = new VocabularyDAOFileImpl(workingDirectory.getName());
+        vocabularyID = workingDirectory.listFiles().length;
+        vocabularyID = getLatestID();
+
+        user_info.setVisible(false);
+        ObservableList<String> languages =
+                FXCollections.observableArrayList("German", "English");
+
+        from_choice.setItems(languages);
+        to_choice.setItems(languages);
+
+        HashMap<String, Locale> language_map = new HashMap<String, Locale>();
+        language_map.put("German", Locale.GERMAN);
+        language_map.put("English", Locale.ENGLISH);
+
+        from_choice.valueProperty().addListener((observableValue, oldValue, newValue) -> {
+            from_label.setText(newValue + ":");
+
+            if(!to_choice.getSelectionModel().isEmpty()  &&
+                    (to_choice.getSelectionModel().getSelectedItem().equals(newValue))){
+
+                updateUserInformation("equal_lang");
+            }
+            else{
+                user_info.setVisible(false);
+            }
         });
-        english_field.textProperty().addListener((observable, oldValue, newValue) -> {
-            english_string = newValue;
+
+        to_choice.valueProperty().addListener((observableValue, oldValue, newValue) -> {
+            to_label.setText(newValue + ":");
+
+            if(!from_choice.getSelectionModel().isEmpty() &&
+                    from_choice.getSelectionModel().getSelectedItem().equals(newValue)){
+
+                updateUserInformation("equal_lang");
+            }
+            else{
+                user_info.setVisible(false);
+            }
+        });
+
+        from_field.textProperty().addListener((observable, oldValue, newValue) -> {
+            from_string = newValue;
+        });
+        to_field.textProperty().addListener((observable, oldValue, newValue) -> {
+            to_string = newValue;
         });
 
         submit_btn.setOnAction(new EventHandler<ActionEvent>() {
 
             public void handle(ActionEvent event) {
-                System.out.println(german_string + " " + english_string); // replace with add vocab
+                if(from_choice.getSelectionModel().isEmpty()
+                   || to_choice.getSelectionModel().isEmpty()
+                   || from_field.getText().isEmpty() || to_field.getText().isEmpty()){
+                    updateUserInformation("missing_param");
+                }
+                else if(from_choice.getSelectionModel().getSelectedItem().equals(to_choice.getSelectionModel().getSelectedItem())){
+                    updateUserInformation("equal_lang");
+                }
+                else{
+                    user_info.setVisible(false);
+
+                    vocabularyID++;
+
+                    String from_lang = from_choice.getSelectionModel().getSelectedItem();
+                    String to_lang = to_choice.getSelectionModel().getSelectedItem();
+
+                    Locale source_lang = language_map.get(from_lang);
+                    Locale target_lang = language_map.get(to_lang);
+
+                    Vocabulary vocab = new Vocabulary(vocabularyID, "My_Vocabs", source_lang, target_lang);
+                    Entry entry = new Entry(from_string, to_string);
+                    vocab.addPhrase(entry);
+                    vocabularyDAO.addVocabulary(vocab);
+                    updateUserInformation("added_vocab");
+                }
             }
         });
 
         return_btn.setOnAction(new EventHandler<ActionEvent>() {
 
             public void handle(ActionEvent event) {
-                System.out.println("Return"); // replace with return to overview(?) tab
+                from_choice.getSelectionModel().clearSelection();
+                to_choice.getSelectionModel().clearSelection();
+                from_field.clear();
+                to_field.clear();
+                from_label.setText("From:");
+                to_label.setText("To:");
+                Controller con = new Controller();
+                getScene().setRoot(con);
             }
         });
+
+
+    }
+
+    private void updateUserInformation(String code){
+        user_info.setVisible(true);
+        user_info.setTextFill(Color.RED);
+        switch (code){
+            case "added_vocab":
+                user_info.setText("Vocabulary added!");
+                break;
+            case "equal_lang":
+                user_info.setText("Please select another language");
+                break;
+            case "missing_param":
+                user_info.setText("Please fill out everything");
+                break;
+            default:
+                user_info.setText("Sorry, something went wrong");
+        }
+    }
+
+    private int getLatestID(){
+
+        File[] files = workingDirectory.listFiles(File::isFile);
+        final String[] token = {null};
+        final int[] id = {0};
+
+        id_task = new Task<Integer>() {
+            @Override
+            protected Integer call() throws Exception {
+                if (files != null) {
+                    for (File file : files) {
+
+                        token[0] = file.getName().split("\\.")[0];
+
+                        if (id[0] < Integer.parseInt(token[0]))
+                            id[0] = Integer.parseInt(token[0]);
+
+                    }
+                    return id[0];
+                }
+                return 1;
+            }
+        };
+
+        id_task.stateProperty().addListener(((observableValue, oldState, newState) -> {
+            if(newState == Worker.State.CANCELLED){
+                id_task = null;
+            }
+        }
+        ));
+
+        Thread th = new Thread(id_task);
+        th.setDaemon(true);
+        th.start();
+         //ID = 1 if no files exist
+        return 1;
     }
 }
--- src/main/java/at/tugraz/sw20asd/lang/ui/Controller.java
+++ src/main/java/at/tugraz/sw20asd/lang/ui/Controller.java
@@ -1,5 +1,9 @@
 package at.tugraz.sw20asd.lang.ui;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
@@ -7,15 +11,28 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Collection;
 
 
 public class Controller extends VBox {
+    @FXML
+    private Button overview_btn;
+    @FXML
+    private Button add_btn;
+    @FXML
+    private Button train_btn;
+    @FXML
+    private Button exit_btn;
+
+    FXMLLoader loader = new FXMLLoader();
+
 
 
     public Controller() {
-        FXMLLoader loader = new FXMLLoader();
+
         loader.setControllerFactory(c -> this);
         loader.setRoot(this);
         try {
@@ -25,7 +42,34 @@ public class Controller extends VBox {
             throw new RuntimeException(ex);
         }
 
-        this.getChildren().add(new Layout());
+        // this.getChildren().add(new Layout());
+    }
+    public void initialize(){
 
+        add_btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                AddVocab add = new AddVocab();
+                getScene().setRoot(add);
+            }
+        });
+
+        overview_btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("overview");
+            }
+        });
+
+        train_btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("vocab trainer");
+            }
+        });
+
+        exit_btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 }
