af963b8096e5425a8868793253bbd9ea46655de8
Michael Schiller <schillerm01+git@gmail.com>
2020-04-05 20:52:15 +0200

    Add failing test

--- src/test/java/at/tugraz/sw20asd/lang/server/VocabularyControllerTests.java
+++ src/test/java/at/tugraz/sw20asd/lang/server/VocabularyControllerTests.java
@@ -6,13 +6,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,6 +46,57 @@ public class VocabularyControllerTests {
 		assertAll(
 				() -> assertEquals(201, result.getStatusCodeValue()),
 				() -> assertTrue(result.getHeaders().containsKey("Location")));
+	}
+
+	@Test
+	public void testGetAllVocabularies() throws URISyntaxException {
+		URI uri = new URI(String.format("http://localhost:%d/vocab/", randomServerPort));
+
+		ResponseEntity<List<Vocabulary>> response = restTemplate.exchange(
+				uri,
+				HttpMethod.GET,
+				null,
+				new ParameterizedTypeReference<List<Vocabulary>>() {
+				});
+
+		List<Vocabulary> list = response.getBody();
+
+		assertNotNull(list);
+		assertAll(
+				() -> assertFalse(list.isEmpty()),
+				() -> assertEquals(2, list.size()));
+	}
+
+	@Test
+	public void testGetVocabularyById_ExistingId() throws URISyntaxException {
+		URI uri = new URI(String.format("http://localhost:%d/vocab/1", randomServerPort));
+
+		ResponseEntity<Vocabulary> result = restTemplate.getForEntity(uri, Vocabulary.class);
+
+		assertAll(
+				() -> assertEquals(HttpStatus.OK, result.getStatusCode()),
+				() -> assertTrue(result.hasBody()));
+
+		Vocabulary vocab = result.getBody();
+		assertAll(
+				() -> assertNotNull(vocab),
+				() -> assertEquals(1, vocab.getID()));
+	}
+
+	@Test
+	public void testGetVocabularyById_NonExistingId() throws URISyntaxException {
+		URI uri = new URI(String.format("http://localhost:%d/vocab/42", randomServerPort));
+
+		assertThrows(HttpClientErrorException.NotFound.class,
+				() -> restTemplate.getForEntity(uri, Vocabulary.class));
+	}
+
+	@Test
+	public void testGetVocabularyById_MalformedId() throws URISyntaxException {
+		URI uri = new URI(String.format("http://localhost:%d/vocab/foo", randomServerPort));
+
+		assertThrows(HttpClientErrorException.BadRequest.class,
+				() -> restTemplate.getForEntity(uri, Vocabulary.class));
 	}
 
 	private HttpEntity<Vocabulary> CreateVocabularyEntity(String vocabularyName) {
