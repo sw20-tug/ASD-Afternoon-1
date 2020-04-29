ae4fd7f06ccd31bda2a1193236e40180d51a2203
Sergio Calvo Monasterio <100366702@alumnos.uc3m.es>
2020-04-29 19:21:25 +0200

    LANG_002b: Connected Overview of Vocabs and of words [CS,LA]

--- src/main/java/at/tugraz/sw20asd/lang/ui/Controller/OverviewVocabs.java
+++ src/main/java/at/tugraz/sw20asd/lang/ui/Controller/OverviewVocabs.java
@@ -9,14 +9,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
+
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
-
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class OverviewVocabs extends VBox {
 
@@ -29,7 +33,7 @@ public class OverviewVocabs extends VBox {
 
 
     private VocabularyAccess vocab;
-
+    private int i;
     String jsonFilesFolder = System.getProperty("user.dir") + "/vocabs";
 
 
@@ -49,10 +53,33 @@ public class OverviewVocabs extends VBox {
 
 
 
-    public void initialize() {
+    public void initialize() throws FileNotFoundException {
 
         int num_but = new File(jsonFilesFolder).list().length;
-        for (int i = 0; i < num_but; i++) {
+        ArrayList list = new ArrayList();
+
+
+
+
+        for (i = 0; i < num_but; i++) {
+            String NameVocab = "";
+            JSONParser jsonParser = new JSONParser();
+            list.add(i);
+            try (FileReader reader = new FileReader(jsonFilesFolder + "/"+ i + ".vocab"))
+            {
+                //Read JSON file
+                Object obj = jsonParser.parse(reader);
+
+                JSONObject json = (JSONObject) obj;
+
+
+                NameVocab = parseNameObject(json);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             Button b = new Button("b" + String.valueOf(i));
             b.setId("b" + String.valueOf(i));
             b.setPrefHeight(40.0);
@@ -61,11 +88,27 @@ public class OverviewVocabs extends VBox {
             b.setFont(new Font("Abyssinica SIL", 16));
             b.setLayoutX(318.0);
             b.setLayoutY(300.0);
+            b.setText(NameVocab);
+
+            b.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                    OverviewWords overview = new OverviewWords(vocab, list);
+                    getScene().setRoot(overview);
+                }
+            });
             getChildren().add(b);
         }
 
 
 
 
+    }
+
+    private static String parseNameObject(JSONObject vocab)
+    {
+
+        String NameVocab = (String) vocab.get("name");
+
+        return NameVocab;
     }
 }
--- src/main/java/at/tugraz/sw20asd/lang/ui/Controller/OverviewWords.java
+++ src/main/java/at/tugraz/sw20asd/lang/ui/Controller/OverviewWords.java
@@ -10,9 +10,13 @@ import javafx.scene.layout.VBox;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import at.tugraz.sw20asd.lang.model.Entry;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class OverviewWords extends VBox {
 
@@ -23,15 +27,18 @@ public class OverviewWords extends VBox {
     @FXML
     private Button edit_btn;
     @FXML
-    private Button vocabtrainer_btn;
+    private Button return_btn;
     @FXML
     private TableView<String> table;
 
     private VocabularyAccess vocab;
+    private ArrayList list;
+    String jsonFilesFolder = System.getProperty("user.dir") + "/vocabs";
 
 
-    public OverviewWords(VocabularyAccess vocab){
-        this.vocab= vocab;
+    public OverviewWords(VocabularyAccess vocab, ArrayList list){
+        this.list = list;
+        this.vocab = vocab;
         FXMLLoader loader = new FXMLLoader();
         loader.setControllerFactory(c -> this);
         loader.setRoot(this);
@@ -43,6 +50,34 @@ public class OverviewWords extends VBox {
         }
     }
     public void initialize(){
+        String SourceLanguage = "";
+        String TargetLanguage= "";
+        JSONParser jsonParser = new JSONParser();
+        for (int x = 0; x < list.size(); x++){
+            try (FileReader reader = new FileReader(jsonFilesFolder + "/"+ list.get(x) + ".vocab"))
+            {
+                //Read JSON file
+                Object obj = jsonParser.parse(reader);
+
+                JSONObject json = (JSONObject) obj;
+
+
+                SourceLanguage = parseSourceLanguageObject(json);
+                TargetLanguage = parseTargetLanguageObject(json);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+
+        TableColumn sourceLanguage = new TableColumn(SourceLanguage);
+        TableColumn targetLanguage = new TableColumn(TargetLanguage);
+        table.getColumns().addAll(sourceLanguage, targetLanguage);
+
+
+
 
         add_btn.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -60,7 +95,7 @@ public class OverviewWords extends VBox {
             }
         });
 
-        vocabtrainer_btn.setOnAction(new EventHandler<ActionEvent>() {
+        return_btn.setOnAction(new EventHandler<ActionEvent>() {
 
             public void handle(ActionEvent event) {
                 Controller vocab_menu = new Controller();
@@ -68,5 +103,21 @@ public class OverviewWords extends VBox {
             }
         });
 
+    }
+
+    private static String parseSourceLanguageObject(JSONObject vocab)
+    {
+
+        String SourceLanguage = (String) vocab.get("sourceLanguage");
+
+        return SourceLanguage;
+    }
+
+    private static String parseTargetLanguageObject(JSONObject vocab)
+    {
+
+        String TargetLanguage = (String) vocab.get("targetLanguage");
+
+        return TargetLanguage;
     }
 }
