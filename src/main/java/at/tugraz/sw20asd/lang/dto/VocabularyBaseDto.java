d7744878a2c9da7c023670b0adcce46f58004e31
Michael Schiller <schillerm01+git@gmail.com>
2020-05-13 15:28:54 +0200

    LANG_003d Introduce data transfer classes [CS,SM]

---
+++ src/main/java/at/tugraz/sw20asd/lang/dto/VocabularyBaseDto.java
@@ -0,0 +1,60 @@
+package at.tugraz.sw20asd.lang.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.Locale;
+
+@JsonPropertyOrder({"id", "name", "sourceLanguage", "targetLanguage"})
+public class VocabularyBaseDto {
+    private Long _id;
+
+    private String _name;
+
+    private Locale _sourceLanguage;
+
+    private Locale _targetLanguage;
+
+    public VocabularyBaseDto() {
+        super();
+    }
+
+    public VocabularyBaseDto(long id, String name, Locale src, Locale dst) {
+        super();
+        _id = id;
+        _name = name;
+        _sourceLanguage = src;
+        _targetLanguage = dst;
+    }
+
+    public Long getId() {
+        return _id;
+    }
+
+    public void setId(Long id) {
+        _id = id;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+
+    public Locale getSourceLanguage() {
+        return _sourceLanguage;
+    }
+
+    public void setSourceLanguage(Locale lang) {
+        _sourceLanguage = lang;
+    }
+
+    public Locale getTargetLanguage() {
+        return _targetLanguage;
+    }
+
+    public void setTargetLanguage(Locale lang) {
+        _targetLanguage = lang;
+    }
+}
