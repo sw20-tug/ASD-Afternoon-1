3013fede428cd76cc5040f10c2e92ab4cdf102be
Wis-Ali <56490018+Wis-Ali@users.noreply.github.com>
2020-06-02 19:33:29 +0200

    LANG_004a Adjusted StudyVocab to VocabularySelectionModel [BW,DE]

--- lang-ui/src/main/java/at/tugraz20/sw20asd/lang/ui/controllers/StudyVocab.java
+++ lang-ui/src/main/java/at/tugraz20/sw20asd/lang/ui/controllers/StudyVocab.java
@@ -4,7 +4,11 @@ import at.tugraz.sw20asd.lang.dto.EntryDto;
 import at.tugraz.sw20asd.lang.dto.VocabularyDetailDto;
 import at.tugraz.sw20asd.lang.ui.VocabularyAccess;
 import at.tugraz.sw20asd.lang.ui.models.VocabularySelectionModel;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
@@ -13,13 +17,10 @@ import javafx.scene.layout.*;
 import javafx.scene.layout.VBox;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import javafx.scene.paint.Color;
+import java.util.*;
 
 public class StudyVocab extends VBox {
     @FXML
@@ -49,10 +50,17 @@ public class StudyVocab extends VBox {
     @FXML
     private Button start_btn;
 
-    private List<Button> answer_button_list = new ArrayList<Button>();
-    private List<Label> given_label_list = new ArrayList<Label>();
-    private List<Label> answer_label_list = new ArrayList<Label>();
+    private List<Button> answer_button_list = new ArrayList<>();
+    private List<Label> given_label_list = new ArrayList<>();
+    private List<Label> answer_label_list = new ArrayList<>();
+
+    private List<String> languages_list = new ArrayList<>();
+    private String language1;
+    private String language2;
     private List<EntryDto> words = new ArrayList<>();
+
+    private Task<List<VocabularyDetailDto>> getVocabsTask;
+    private List<VocabularyDetailDto> vocabulary_list = new ArrayList<>();
 
     private VocabularyAccess vocab;
     private ObservableList<VocabularySelectionModel> voc;
@@ -72,155 +80,194 @@ public class StudyVocab extends VBox {
         }
     }
 
-        public void initialize() {
-            user_info.setVisible(false);
-            study_label.setText(study_language);
-            show_all_btn.setVisible(false);
-            switch_btn.setVisible(false);
-            given_label.setVisible(false);
-            study_label.setVisible(false);
-            start_btn.setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent event) {
-                    if(language_choice.getSelectionModel().isEmpty()){
-                        user_info.setVisible(true);
-                        user_info.setText("Please choose a language!");
-                    }
-                    else {
-                        study_language = language_choice.getSelectionModel().getSelectedItem();
-                        language_choice.getSelectionModel().isEmpty();
-                        study_label.setText(study_language);
-                        for(int counter = 0; counter < voc.size(); counter++){
-                            if(voc.get(counter).getSourceLanguage().toString().equals("de")){
-                                for(EntryDto e : voc.get(counter).getEntries()){
-                                    words.add(e);
-                                }
-                            }
-                            else{
-                                for(EntryDto e : voc.get(counter).getEntries()){
-                                    EntryDto e_convert = new EntryDto(e.getTranslation(),e.getPhrase());
-                                    words.add(e_convert);
-                                }
-                            }
-                        }
-                        populate();
-                    }
-                }
-            });
+    public void initialize() {
+        user_info.setVisible(false);
+        study_label.setText(study_language);
+        show_all_btn.setVisible(false);
+        switch_btn.setVisible(false);
+        given_label.setVisible(false);
+        study_label.setVisible(false);
+        language1 = voc.get(0).getVocabularySrc();
+        language2 = voc.get(0).getVocabularyTarget();
+        languages_list.add(language1);
+        languages_list.add(language2);
+        ObservableList<String> languages = FXCollections.observableList(languages_list);
+        language_choice.setItems(languages);
 
-            return_btn.setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent event) {
-                    OverviewVocabs overview = new OverviewVocabs(vocab);
-                    getScene().setRoot(overview);
-                }
-            });
-
-        }
-
-        private void showAllAnswers(){
-                if(show_all_btn.getText().equals("Show All Answers")){
-                    for(int counter = 0; counter < given_label_list.size(); counter++) {
-                        answer_label_list.get(counter).setVisible(true);
-                        answer_button_list.get(counter).setText("Hide Answer");
-                        show_all_btn.setText("Hide All Answers");
-                    }
-                }
-                else{
-                    for(int counter = 0; counter < given_label_list.size(); counter++) {
-                        answer_label_list.get(counter).setVisible(false);
-                        answer_button_list.get(counter).setText("Show Answer");
-                        show_all_btn.setText("Show All Answers");
-                    }
+        start_btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                if (language_choice.getSelectionModel().isEmpty()) {
+                    user_info.setVisible(true);
+                    user_info.setText("Please choose a language!");
+                } else {
+                    study_language = language_choice.getSelectionModel().getSelectedItem();
+                    study_label.setText(study_language);
+                    getEntryList();
                 }
             }
+        });
 
-        private void switchLanguage(){
-            show_all_btn.setText("Hide All Answers");
-            showAllAnswers();
-            for(int counter = 0; counter < given_label_list.size(); counter++)
-            {
-                String new_answer = given_label_list.get(counter).getText();
-                String new_given = answer_label_list.get(counter).getText();
-                String new_given_language = study_label.getText();
-                String new_study_language = given_label.getText();
-
-                given_label_list.get(counter).setText(new_given);
-                answer_label_list.get(counter).setText(new_answer);
-                given_label.setText(new_given_language);
-                study_label.setText(new_study_language);
+        return_btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                OverviewVocabs overview = new OverviewVocabs(vocab);
+                getScene().setRoot(overview);
             }
-        }
+        });
 
-        private void populate(){
-            user_info.setVisible(false);
-            show_all_btn.setVisible(true);
-            switch_btn.setVisible(true);
-            given_label.setVisible(true);
-            study_label.setVisible(true);
-            anchor_pane.getChildren().remove(choice_label);
-            anchor_pane.getChildren().remove(language_choice);
-            anchor_pane.getChildren().remove(start_btn);
+    }
 
-            int min_height = 30;
-            int margin = 10;
-            for (int counter = 0; counter < words.size(); counter++) {
-
-                String given_str;
-                String answer_str;
-                if(study_language.equals("de")){
-                    given_str = words.get(counter).getTranslation();
-                    answer_str = words.get(counter).getPhrase();
-                }
-                else{
-                    given_str = words.get(counter).getPhrase();
-                    answer_str = words.get(counter).getTranslation();
-                }
-
-                Label given = new Label(given_str);
-                given.setId("given" + given_label_list.size());
-                given.setMinHeight(min_height);
-                setMargin(given, new Insets(10,0,0,0));
-                given_label_list.add(given);
-                given_list.getChildren().add(given);
-
-                Label answer = new Label(answer_str);
-                answer.setId("answer" + answer_label_list.size());
-                answer.setVisible(false);
-                answer.setMinHeight(min_height);
-                setMargin(answer, new Insets(10,0,0,0));
-                answer_label_list.add(answer);
-                answer_list.getChildren().add(answer);
-
-                Button show_answer = new Button();
-                show_answer.setText("Show Answer");
-                show_answer.setId("btn" + answer_button_list.size());
-                show_answer.setMinHeight(min_height);
-                setMargin(show_answer, new Insets(10,0,0,0));
-                show_answer.setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent event) {
-                        if(show_answer.getText().equals("Show Answer")){
-                            answer.setVisible(true);
-                            show_answer.setText("Hide Answer");
-                        }
-                        else {
-                            answer.setVisible(false);
-                            show_answer.setText("Show Answer");
-                        }
-                    }
-                });
-                answer_button_list.add(show_answer);
-                show_answer_list.getChildren().add(show_answer);
+    private void showAllAnswers() {
+        if (show_all_btn.getText().equals("Show All Answers")) {
+            for (int counter = 0; counter < given_label_list.size(); counter++) {
+                answer_label_list.get(counter).setVisible(true);
+                answer_button_list.get(counter).setText("Hide Answer");
+                show_all_btn.setText("Hide All Answers");
             }
-
-            switch_btn.setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent event) {
-                    switchLanguage();
-                }
-            });
-
-            show_all_btn.setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent event) {
-                    showAllAnswers();
-                }
-            });
+        } else {
+            for (int counter = 0; counter < given_label_list.size(); counter++) {
+                answer_label_list.get(counter).setVisible(false);
+                answer_button_list.get(counter).setText("Show Answer");
+                show_all_btn.setText("Show All Answers");
+            }
         }
     }
+
+    private void switchLanguage() {
+        show_all_btn.setText("Hide All Answers");
+        showAllAnswers();
+        for (int counter = 0; counter < given_label_list.size(); counter++) {
+            String new_answer = given_label_list.get(counter).getText();
+            String new_given = answer_label_list.get(counter).getText();
+            String new_given_language = study_label.getText();
+            String new_study_language = given_label.getText();
+
+            given_label_list.get(counter).setText(new_given);
+            answer_label_list.get(counter).setText(new_answer);
+            given_label.setText(new_given_language);
+            study_label.setText(new_study_language);
+        }
+    }
+
+    private void populate() {
+        user_info.setVisible(false);
+        show_all_btn.setVisible(true);
+        switch_btn.setVisible(true);
+        given_label.setVisible(true);
+        study_label.setVisible(true);
+        anchor_pane.getChildren().remove(choice_label);
+        anchor_pane.getChildren().remove(language_choice);
+        anchor_pane.getChildren().remove(start_btn);
+
+        int min_height = 30;
+        int margin = 10;
+        for (int counter = 0; counter < words.size(); counter++) {
+
+            String given_str;
+            String answer_str;
+            if (study_language.equals("de")) {
+                given_str = words.get(counter).getTranslation();
+                answer_str = words.get(counter).getPhrase();
+            } else {
+                given_str = words.get(counter).getPhrase();
+                answer_str = words.get(counter).getTranslation();
+            }
+
+            Label given = new Label(given_str);
+            given.setId("given" + given_label_list.size());
+            given.setMinHeight(min_height);
+            setMargin(given, new Insets(margin, 0, 0, 0));
+            given_label_list.add(given);
+            given_list.getChildren().add(given);
+
+            Label answer = new Label(answer_str);
+            answer.setId("answer" + answer_label_list.size());
+            answer.setVisible(false);
+            answer.setMinHeight(min_height);
+            setMargin(answer, new Insets(10, 0, 0, 0));
+            answer_label_list.add(answer);
+            answer_list.getChildren().add(answer);
+
+            Button show_answer = new Button();
+            show_answer.setText("Show Answer");
+            show_answer.setId("btn" + answer_button_list.size());
+            show_answer.setMinHeight(min_height);
+            setMargin(show_answer, new Insets(10, 0, 0, 0));
+            show_answer.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                    if (show_answer.getText().equals("Show Answer")) {
+                        answer.setVisible(true);
+                        show_answer.setText("Hide Answer");
+                    } else {
+                        answer.setVisible(false);
+                        show_answer.setText("Show Answer");
+                    }
+                }
+            });
+            answer_button_list.add(show_answer);
+            show_answer_list.getChildren().add(show_answer);
+        }
+
+        switch_btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                switchLanguage();
+            }
+        });
+
+        show_all_btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                showAllAnswers();
+            }
+        });
+    }
+
+    private void getEntryList() {
+        //get Vocabulary group
+
+        getVocabsTask = new Task<List<VocabularyDetailDto>>() {
+            @Override
+            protected List<VocabularyDetailDto> call() throws Exception {
+                for(VocabularySelectionModel Model : voc) {
+                    vocabulary_list.add(vocab.getVocabulary(Model.getVocabularyId()));
+                }
+                return vocabulary_list;
+            }
+        };
+
+        getVocabsTask.stateProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue == Worker.State.CANCELLED || newValue == Worker.State.FAILED) {
+                Platform.runLater(() -> {
+                    updateUserInformation("");
+                });
+                getVocabsTask.cancel();
+            }
+
+            if (newValue == Worker.State.SUCCEEDED) {
+                if (vocabulary_list == null) {
+                    Platform.runLater(() -> {
+                        updateUserInformation("No words added");
+                    });
+                } else {
+                    Platform.runLater(() -> {
+                        for (VocabularyDetailDto v : vocabulary_list) {
+                            for (EntryDto e : v.getEntries()) {
+                                if (v.getSourceLanguage().toString().equals(language1)) {
+                                    words.add(e);
+                                } else {
+                                    words.add(new EntryDto(e.getTranslation(), e.getPhrase()));
+                                }
+                            }
+                        }
+                        System.out.println(words);
+                        populate();
+                    });
+                }
+            }
+        }));
+        Thread th = new Thread(getVocabsTask);
+        th.setDaemon(true);
+        th.start();
+    }
+
+    private void updateUserInformation(String s) {
+    }
+}
